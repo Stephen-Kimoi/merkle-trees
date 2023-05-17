ab673dd0f83030af1dabd2cfd3058acc2dac9d44
Steve Kimoi <moenisteve@gmail.com>
2023-05-17 08:44:09 +0300

    adding function for converting bytes array to bytes 32 array

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -12,8 +12,23 @@ contract Whitelist {
 
     function checkInWhitelist(bytes[] calldata proof, uint64 maxAllowanceToMint) view public returns(bool){
        bytes32 leaf = keccak256(abi.encode(msg.sender, maxAllowanceToMint)); 
+       bytes32[] memory convertedProof = convertBytesArrayToBytes32Array(proof);
+       bool verified = MerkleProof.verify(convertedProof, merkleRoot, leaf);
        bool verified = MerkleProof.verify(proof, merkleRoot, leaf); 
        return verified; 
+    }
+    
+    function convertBytesArrayToBytes32Array(bytes[] calldata data) internal pure returns (bytes32[] memory) {
+        bytes32[] memory result = new bytes32[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+        require(data[i].length == 32, "Invalid element length");
+        bytes32 convertedData;
+        assembly {
+            convertedData := mload(add(data[i], 0x20))
+        }
+        result[i] = convertedData;
+}
+        return result;
     }
 
 
