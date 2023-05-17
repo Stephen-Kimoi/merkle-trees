3fba35ba4a28ad0a17169972034cae7c634d93a0
Steve Kimoi <moenisteve@gmail.com>
2023-05-17 08:44:56 +0300

    testing whether the contract works or not

--- test/Merkle-root.js
+++ test/Merkle-root.js
@@ -11,5 +11,40 @@ function encodeLeaf(address, spots){
 }
 
 describe("Merkle Trees", function() {
-    
+    it ("SHould be able to verify if address is in whitelist or not", async function () {
+
+       const testAddresses = await ethers.getSigners(); 
+
+       const list = [
+          encodeLeaf(testAddresses[0], 2),
+          encodeLeaf(testAddresses[1], 2),
+          encodeLeaf(testAddresses[2], 2),
+          encodeLeaf(testAddresses[3], 2),
+          encodeLeaf(testAddresses[4], 2),
+          encodeLeaf(testAddresses[5], 2)
+       ]
+
+       const merkleTree = new MerkleTree(list, keccak256, { hashLeaves: true, sortPairs: true, sortLeaves: true })
+
+       const root = merkleTree.getHexRoot(); 
+
+       const whitelist = await ethers.getContractFactory("Whitelist"); 
+       const Whitelist = await whitelist.deploy(root);  
+
+       // Checking for valid addresses 
+       for (let i = 0; i < 6; i++){
+          const leaf = keccak256(list[i]); 
+          const proof = merkleTree.getHexProof(leaf); 
+
+          const connectedWhiteList = await Whitelist.connect(testAddresses[i])
+
+          const verified = await connectedWhiteList.checkInWhitelist(proof, 2); 
+          expect(verified).to.equal(true); 
+       }
+
+       // Checking for invalid addresses 
+       const verifiedInvalid = await Whitelist.checkInWhitelist([], 2); 
+       expect(verifiedInvalid).to.equal(false); 
+           
+    })
 })
